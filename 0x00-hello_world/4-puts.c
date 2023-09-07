369ef5779a732d2832e80e52a3b04b056d576bc2
Globski <gloria.ogunsemore@gmail.com>
2023-09-07 16:18:50 -0700

    Update-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+ *main - main entry point function
  *
  *return - always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
