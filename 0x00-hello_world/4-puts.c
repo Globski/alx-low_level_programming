45646293467f7cc9ea35361bce1204faaf0eedd1
Globski <gloria.ogunsemore@gmail.com>
2023-09-07 16:59:18 -0700

    Update-5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - main entry point function
+ * main - main entry point
  *
- *return - always 0 (success)
+ * Return: Always return 0. (success)
  */
 int main(void)
 {
