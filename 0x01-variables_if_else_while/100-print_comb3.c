b9b61517e5474651403e718b5b6bb4efc6886ed3
Globski <gloria.ogunsemore@gmail.com>
2023-09-09 21:20:22 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Mai entry point
+ *
+ * Description: Print all possible different combinations of two digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int digit_3 = 48;
+	int digit_2;
+
+	while (digit_3 <= 56)
+{
+		digit_2 = digit_3 + 1;
+
+	while (digit_2 <= 57)
+{
+		putchar(digit_3);
+		putchar(digit_2);
+
+	if (digit_3 < 56)
+{
+		putchar(',');
+		putchar(' ');
+}
+		digit_2++;
+}
+		digit_3++;
+}
+		putchar('\n');
+	return (0);
+}
