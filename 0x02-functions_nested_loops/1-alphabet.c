dfa857431fdddff13e76cf10a4978530ef4088c2
Globski <gloria.ogunsemore@gmail.com>
2023-09-12 12:46:48 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Main entry point
+ *
+ * Description: A function that prints the alphabet, in
+ * lowercase, followed by a new line.
+ *
+ * Return: Always 0
+*/
+
+void print_alphabet(void);
+
+int main(void)
+{
+	char alphabet_l = 'a';
+
+	while (alphabet_l <= 'z')
+{
+		_putchar(alphabet_l);
+		alphabet_l++;
+}
+		_putchar('\n');
+
+	return (0);
+}
