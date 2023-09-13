dd7967db43947fd6d6b870790c5ca2f2c888f3f1
Globski <gloria.ogunsemore@gmail.com>
2023-09-13 13:13:57 -0700

    Hot fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (digit_1 = 0; digit_1 <= 98; digit_1++)
 {
-	for (digit_2 = digit_1 + 1; digit_2 <= 99; digit_2++)
+		for (digit_2 = digit_1 + 1; digit_2 <= 99; digit_2++)
 {
 			putchar((digit_1 / 10) + '0');
 			putchar((digit_1 % 10) + '0');
