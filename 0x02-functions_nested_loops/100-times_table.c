05c6ac0ff298b40e40e36e9eb58b5ff8fe382468
Globski <gloria.ogunsemore@gmail.com>
2023-09-14 03:08:57 -0700

    debugging

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,28 +15,31 @@ void print_times_table(int n)
 	value = 0;
 		while (value <= n)
 {
-			value++;
-			_putchar('0');
-	sig = 1;
+	sig = 0;
 
 		while (sig <= n)
 
 {
-			sig++;
-			_putchar(',');
-			_putchar(' ');
-
 	sum = value * sig;
 
-					if (sum <= 99)
-						_putchar(' ');
-					if (sum <= 9)
-						_putchar(' ');
 
+					if (sum < 10)
+						_putchar(sum + '0');
+} 
+					else
+{
+						_putchar((sum / 10) + '0');
+						_putchar((sum % 10) + '0');
+}
+					if (sig < n)
+{
+						_putchar(' ');
+						_putchar(',');
+}
 					if (sum >= 100)
 {
 						_putchar((sum / 100) + '0');
-						_putchar(((sum / 10)) % 10 + '0');
+						_putchar(((sum / 10)) % 10 + '0')
 }
 				else if (sum <= 99 && sum >= 10)
 {
