9c6964905bbc6e308675089b4427163c6de7e3be
Globski <gloria.ogunsemore@gmail.com>
2023-09-14 13:53:41 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - A function that prints the
+ * numbers, from 0 to 9, followed by a new line.
+ * excluding print 2 and 4.
+ *
+ */
+
+void print_most_numbers(void)
+{
+
+	int numb;
+
+	for (numb = 48; numb <= 57; numb++)
+	{
+		if (numb == 50 || numb == 52)
+		{
+
+			continue;
+			_putchar(numb);
+		}
+		else
+		{
+			_putchar(numb);
+		}
+	}
+	_putchar('\n');
+}
