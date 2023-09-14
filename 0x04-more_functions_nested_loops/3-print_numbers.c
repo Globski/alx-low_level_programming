ac537fe265d51bae2d3497690d9fb2699d245acf
Globski <gloria.ogunsemore@gmail.com>
2023-09-14 13:20:59 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - A function that prints the 
+ * numbers, from 0 to 9, followed by a new line.
+ *
+ */
+
+void print_numbers(void)
+{
+
+	int numb;
+	for (numb = 48; numb <= 57; numb++)
+	{
+		_putchar(numb);
+	}
+	_putchar('\n');
+}
