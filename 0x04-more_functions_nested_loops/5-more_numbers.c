8d905942dabd1581bbe4c8a37e49f2f91d5ba29f
Gloriana <141042662+Globski@users.noreply.github.com>
2023-09-15 02:19:40 -0700

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,15 @@ void more_numbers(void)
 
 	int numb, numb2;
 
-	for (numb2 = 0; numb2 < 10; numb2++)
+	for (numb = 0; numb < 10; numb++)
 	{
-		for (numb = 0; numb <= 15; numb++)
+		for (numb2 = 0; numb2 <= 15; numb2++)
 		{
-			if (numb >= 10)
+			if (numb2 >= 10)
 			{
-				_putchar('0' + (numb / 10));
+				_putchar(numb2 / 10 + '0');
 			}
-			_putchar('0' + (numb % 10));
+			_putchar(numb2 % 10 + '0');
 		}
 		_putchar('\n');
 	}
