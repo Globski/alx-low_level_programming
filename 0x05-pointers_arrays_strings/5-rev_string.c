202c130d4da983973a467898e66601715ff624a8
Globski <gloria.ogunsemore@gmail.com>
2023-09-18 13:45:13 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_rev - A function that prints a string, in reverse
+ * @s: The string to print
+ */
+void rev_string(char *s) 
+{
+	int str_len = 0, arr_loc = 0, hold;
+
+	while (s[str_len] != '\0')
+	{
+		str_len++;
+	}
+
+	for (arr_loc = 0; arr_loc < str_len / 2; arr_loc++)
+	{
+		hold = s[arr_loc];
+		s[arr_loc] = s[str_len - arr_loc - 1];
+		s[str_len - arr_loc - 1] = hold;
+	}
+}
