115890b55d7893376947d687d84f125dc33f7857
Globski <gloria.ogunsemore@gmail.com>
2023-09-18 20:30:49 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,7 +10,13 @@
 int main(void)
 {
 
-	char keys[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY0123456789";
+	char keys[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+	     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+	     'u', 'v', 'w', 'x', 'y', 'z',
+	     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+	     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+	     'U', 'V', 'W', 'X', 'Y', 'Z',
+	     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	const unsigned int PASS_LEN = 60;
 	unsigned int i, arr_loc;
 
