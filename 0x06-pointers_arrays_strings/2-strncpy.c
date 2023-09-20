4b4901275d0150544ad320e107aa22c9bf3e9a94
Globski <gloria.ogunsemore@gmail.com>
2023-09-20 11:40:33 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strncpy - A function that copies a string.
+ *
+ * @dest: The destination string where src will be copied.
+ * @src: The source string to copy from.
+ * @n: The maximum number of characters to copy from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *join = dest;
+
+
+	while (*src != 0 && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+
+	while (n > 0)
+	{
+		*dest = 0;
+		dest++;
+		n--;
+	}
+
+	return (join);
+}
