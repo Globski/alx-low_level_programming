40c7ab2f4fce29ce14f56acc8e9df6649e3d3ef1
Globski <gloria.ogunsemore@gmail.com>
2023-09-21 16:44:44 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * leet - A function that encodes a string into 1337.
+ *
+ * @src: The input string to encode.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *leet(char *str)
+{
+	int i;
+	char *p = str;
+	char *leet_l = "aAeEoOtTlL";
+	char *leet_plm = "4433007711";
+
+	while (*str != '\0')
+	{
+		for (i = 0; leet_l[i] != '\0'; i++)
+		{
+			if (*str == leet_l[i])
+			{
+				*str = leet_plm[i];
+				break;
+			}
+		}
+		str++;
+	}
+
+	return (p);
+}
