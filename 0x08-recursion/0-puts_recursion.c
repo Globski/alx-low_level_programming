980c66e25e3bb860ee94a85496976aadfb9388dd
Globski <gloria.ogunsemore@gmail.com>
2023-09-28 03:48:30 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - Prints a string followed by a new line recursively.
+ *
+ * @s: Pointer to the string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{	
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
