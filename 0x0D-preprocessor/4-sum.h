8c4a1947b8d7d62f7defdb7d68b8dd71dbd450a7
Globski <gloria.ogunsemore@gmail.com>
2023-10-10 06:29:41 -0700

    Hot fix

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -2,7 +2,7 @@
 #define SUM_H
 
 /*
- * Description: Header file that defines a function-like 
+ * Description: Header file that defines a function-like
  * macro SUM(x, y) that computes the sum of the numbers x and y.
  */
 
