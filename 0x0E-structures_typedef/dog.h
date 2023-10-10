30527b16e2926703f622c1e4eaa9eda780da93a5
Globski <gloria.ogunsemore@gmail.com>
2023-10-10 07:01:33 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog with elements.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ *
+ * Description: This structure defines a dog with a name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
