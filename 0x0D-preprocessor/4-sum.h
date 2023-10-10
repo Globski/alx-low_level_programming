e71750377e3d0fc40bad3862fbd4b4484db3d30b
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-10 16:14:02 +0100

    Update 4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -2,8 +2,8 @@
 #define SUM_H
 
 /*
- * Description: Header file that defines a function-like
- * macro SUM(x, y) that computes the sum of the numbers x and y.
+ * Description: Header file that defines a function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y.
  */
 
 #define SUM(x, y) ((x) + (y))
