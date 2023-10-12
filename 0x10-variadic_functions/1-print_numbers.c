e4a6349a37179380e76c0b1f570a50656bf8613f
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-12 09:43:37 -0700

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,9 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		num = va_arg(args, int);
 		printf("%d", num);
 
-		if (separator == NULL)
-			return;
-		if (separator != NULL && cont < (n - 1))
+		if (separator != NULL && cont != (n - 1))
 			printf("%s", separator);
 	}
 	va_end(args);
