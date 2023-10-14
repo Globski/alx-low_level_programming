ea8b22ba80c0fcd5b5dfc8dbfa4dddf2e95c8ad2
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-14 11:39:08 -0700

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -82,7 +82,7 @@ void print_all(const char * const format, ...)
 	
 	while (format && (*(format + cont)))
 	{
-		int cont1 = 0, cont2 = 0;
+		int cont2 = 0;
 
 		while (cont2 < 4 && format[cont] != *funcs[cont2].selector)
 		{
