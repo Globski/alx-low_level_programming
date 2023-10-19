168350e569d5b745c7a4ac3747d9086feb5e11b1
Globski <gloria.ogunsemore@gmail.com>
2023-10-19 12:25:36 -0700

    Implemented function pre_main_msg

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * pre_main_msg- Prints a message before the main function is executed.
+ */
+
+void pre_main_msg(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
