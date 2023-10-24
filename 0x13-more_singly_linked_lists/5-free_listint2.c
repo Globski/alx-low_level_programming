3fe50178eb43bbcfdf2a59e0549bb519a12d44ad
Globski <gloria.ogunsemore@gmail.com>
2023-10-24 00:20:39 -0700

    Implemeted the function free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list and sets the head to NULL.
+ * @head: A pointer to a pointer to the head of the list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (!head)
+		return;
+
+	while (head)
+	{
+		tmp = *head;
+			*head = (*head)->next;
+		free(tmp);
+	}
+
+	*head = NULL;
+}
