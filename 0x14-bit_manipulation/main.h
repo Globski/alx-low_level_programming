f731e5b0d1b1e909e33d6214dac5477d04f04755
Globski <gloria.ogunsemore@gmail.com>
2023-10-26 10:14:01 -0700

    added 'stddef.h' liabry' to main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,8 @@
  * used in the 0x14-bit_manipulation project.
  */
 
+#include <stddef.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
