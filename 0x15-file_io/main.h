f034d390ff63d65283ceac878121f3ed5c4dd9d8
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-30 13:06:55 -0700

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,9 +13,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+
+#define BUFSIZE 1024;
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error(int code, const char *message, const char *file);
+void close_files(int fileDesc_from, int fileDesc_to);
 
 #endif /* MAIN_H */
