a5eba5b20f60c08acd5b53263d3ae41e0fbe9392
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-30 13:17:41 -0700

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#define BUFSIZE 1024;
+#define BUFSIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
