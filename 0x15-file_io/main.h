5cbfb5b19266d9c1d83bbe0898df96c0238b6970
Gloriana <141042662+Globski@users.noreply.github.com>
2023-10-30 11:52:55 -0700

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
