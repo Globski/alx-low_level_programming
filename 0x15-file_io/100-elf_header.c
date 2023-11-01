f678b644ac1ffabb65573c59dff24f702ad26f53
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-01 03:07:09 -0700

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -20,9 +20,11 @@ void print_error(int code, const char *message)
  */
 void print_elf_header(Elf64_Ehdr *header)
 {
+	int i;
+
 	printf("ELF Header:\n");
 	printf("  Magic:   ");
-	for (int i = 0; i < EI_NIDENT; i++)
+	for (i = 0; i < EI_NIDENT; i++)
 	{
 		printf("%02x ", header->e_ident[i]);
 	}
