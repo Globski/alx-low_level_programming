9397a179264adefbbde8968afbb6171dc5d394a7
Gloriana <141042662+Globski@users.noreply.github.com>
2023-11-01 01:04:49 -0700

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -36,14 +36,65 @@ void print_elf_header(Elf64_Ehdr *header)
 			"2's complement, little endian" : "2's complement, big endian");
 	printf("  Version:                           %d (current)\n",
 			header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            %s\n",
-			header->e_ident[EI_OSABI] == ELFOSABI_SYSV ?
-			"UNIX - System V" : " <unknown>");
+	printf("  OS/ABI:                            ")
+	switch (header->e_ident[EI_OSABI])
+	{
+        	case ELFOSABI_SYSV:
+            		printf("UNIX - System V\n");
+            		break;
+		case ELFOSABI_HPUX:
+            		printf("UNIX - HP-UX\n");
+            		break;
+        	case ELFOSABI_NETBSD:
+            		printf("UNIX - NetBSD\n");
+            		break;
+        	case ELFOSABI_LINUX:
+            		printf("UNIX - Linux\n");
+            		break;
+        	case ELFOSABI_SOLARIS:
+            		printf("UNIX - Solaris\n");
+            		break;
+        	case ELFOSABI_IRIX:
+            		printf("UNIX - IRIX\n");
+            		break;
+        	case ELFOSABI_FREEBSD:
+            		printf("UNIX - FreeBSD\n");
+            		break;
+        	case ELFOSABI_TRU64:
+            		printf("UNIX - TRU64\n");
+            		break;
+        	case ELFOSABI_ARM:
+            		printf("ARM\n");
+            		break;
+        	case ELFOSABI_STANDALONE:
+            		printf("Standalone App\n");
+            		break;
+        	default:
+            		printf("<unknown: %x>\n", header->e_ident[EI_OSABI]);
+    	}
 	printf("  ABI Version:                       %d\n",
 			header->e_ident[EI_ABIVERSION]);
-	printf("  Type:                              %s\n",
-			header->e_type == ET_EXEC ?
-			"EXEC (Executable file)" : " <unknown>");
+	printf("  Type:                              ")
+	switch (header->e_type)
+	{
+        	case ET_NONE:
+            		printf("NONE (None)\n");
+            		break;
+        	case ET_REL:
+            		printf("REL (Relocatable file)\n");
+            		break;
+        	case ET_EXEC:
+            		printf("EXEC (Executable file)\n");
+            		break;
+        	case ET_DYN:
+            		printf("DYN (Shared object file)\n");
+            		break;
+        	case ET_CORE:
+            		printf("CORE (Core file)\n");
+            		break;
+        	default:
+            		printf("<unknown: %x>\n", header->e_type);
+    	}
 	printf("  Entry point address:               0x%lx\n",
 			(unsigned long)header->e_entry);
 }
